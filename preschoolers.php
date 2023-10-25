52c596ec456edfaf6aedf88a415ed16ad808091f
AnibeA <80783166+AnibeA@users.noreply.github.com>
2023-10-25 22:36:10 +0100

    Add files via upload

---
+++ preschoolers.php
@@ -0,0 +1,35 @@
+<?php
+include("header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Babies</title>
+</head>
+<body>
+    <main>
+        <header id ="mainHeader">
+            <h1>The Pre-Schoolers Room</h1>
+        </header>
+        <p>At ABCare we have divided the nursey into 4 rooms -babies, wobbblers, toddlers and pre-schoolers. Choosing the perfect nursery for your child is a difficult decision and we are sure we will exceed your expectations and meet all your requirements.</p>
+        <h3>Meal Plans</h3>
+        <p>We provide your child with a scheduled meal plan including breakfast and lunch. We take our children nutrition seriously as it is important to us to maintain a healthy and nurishing envioroment for our children.</p>
+        <h3>Activities</h3>
+        <p>Our team has scheduled activies that focus on our children skills growth and development such as story time, dress up, puzzles and much more.</p>
+        <a href="contact_us.php" style="text-decoration: none;">Contact Us</a>
+        <div id="roomSpec">
+            <ul style="list-style-type: none;" >
+                <li> Times:8am - 6pm</li>
+                <li>Age: 3 -  5 Year Old </li>
+                <li>Class Size: 12</li>
+                <li>Half Day Rate €35</li>
+                <li>Full Day Day Rate €35</li>
+            </ul>
+            <a href="registeration.php" style = "padding: 1rem; background-color:#049733; color:#fff; text-decoration:none; border-radius:20px;">Register</a>
+        </div>
+    </main>
+</body>
+</html>
